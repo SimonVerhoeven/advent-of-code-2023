fbe21b484c19d142f4ccef3054fded07f1a2905a
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-02 15:09:58 +0100

    feat: day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,11 +1,5 @@
 fun main() {
-    val colours = mapOf("red" to 12, "green" to 13, "blue" to 14)
-
-    fun part1(games: List<List<Map<String, Int>>>): String {
-        return games.withIndex().filter {
-            (_, game) -> game.all { bucket -> bucket.all { (colour, cubes) -> colours.getValue(colour) >= cubes } }
-        }.sumOf { (index) -> index + 1 }.toString()
-    }
+    val colouredCubeStock = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
     fun mapCubes(lines: List<String>): List<List<Map<String, Int>>> {
         return lines.map { line ->
@@ -19,9 +13,20 @@ fun main() {
         }
     }
 
+    fun part1(games: List<List<Map<String, Int>>>): String {
+        return games.withIndex().filter {
+            (_, game) -> game.all { bucket -> bucket.all { (colour, cubes) -> colouredCubeStock.getValue(colour) >= cubes } }
+        }.sumOf { (index) -> index + 1 }.toString()
+    }
+
     fun part2(games: List<List<Map<String, Int>>>): String {
-        return "0";
-//        return input.map { replaceNumbers(it).filter { it.isDigit() } }.sumOf { (it.first() + "" + it.last()).toInt() }
+        return games.sumOf {
+            game -> colouredCubeStock.keys.map {
+                game.maxOf {
+                    bucket -> bucket.getValue(it)
+                }
+            }.reduce { acc, curr -> acc * curr }
+        }.toString()
     }
 
     // test if implementation meets criteria from the description, like:
