6628bd26521b58c15ab94db88b6706a5fbd1384a
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-04 17:14:43 +0100

    feat: day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,8 +1,8 @@
 fun main() {
     fun parseInput(input: List<String>): List<Int> {
         return input.map { line ->
-            val (winningNumbers, scratchedNumbers) = line.substringAfter(": ").split(" | ").map {
-                numbers -> numbers.split(" ").filter { it.isNotEmpty() }.mapTo(hashSetOf()) { it.toInt() }
+            val (winningNumbers, scratchedNumbers) = line.substringAfter(": ").split(" | ").map { numbers ->
+                numbers.split(" ").filter { it.isNotEmpty() }.mapTo(hashSetOf()) { it.toInt() }
             }
 
             scratchedNumbers.intersect(winningNumbers).size
@@ -10,17 +10,24 @@ fun main() {
     }
 
     fun part1(input: List<Int>): Int {
-        return input.sumOf { number -> if (0 == number) 0 else 2.pow(number-1)}
+        return input.sumOf { number -> if (0 == number) 0 else 2.pow(number - 1) }
     }
 
-//    fun part2(input: List<String>): Int {
-//    }
+    fun part2(input: List<Int>): Int {
+        val scratchMap = input.indices.associateWith { 1 }.toMutableMap()
+        input.forEachIndexed { index, number ->
+            (index + 1..index + number).forEach {
+                scratchMap[it] = scratchMap.getValue(it) + scratchMap.getValue(index)
+            }
+        }
+        return scratchMap.values.sum()
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = parseInput(readInput("Day04_test"))
     check(part1(testInput) == 13)
 
-    val input = readInput("Day04")
-    part1(parseInput(input)).println()
-//    part2(input).println()
+    val input = parseInput(readInput("Day04"))
+    part1(input).println()
+    part2(input).println()
 }
