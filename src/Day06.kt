2029fe00b6eff87a9177d5076791a76ab763f287
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-06 05:31:08 +0100

    feat: prep for day 6

---
+++ src/Day06.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun part1(input: List<String>): Long {
+        return 0;
+    }
+
+    fun part2(input: List<String>): Long {
+       return 0;
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 35L)
+    check(part1(testInput) == 35L)
+
+    val input = readInput("Day06")
+    part1(input).println()
+    part2(input).println()
+}
