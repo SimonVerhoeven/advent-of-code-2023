56425bf565938e1a705c47bc2de8b03e260a58ba
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-06 19:31:22 +0100

    feat: day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -11,12 +11,19 @@ fun main() {
         return times.zip(distances).fold(1) { acc, (time, distance) -> acc * (0..time).sumOf { (getTime(time, it) > distance).toInt() }.toLong() }
     }
 
+    fun part2(input: List<String>): Long {
+        val time = input[0].removePrefix("Time:").replace(" ", "").toLong()
+        val distance = input[1].removePrefix("Distance:").replace(" ", "").toLong()
+
+        return (0..time).sumOf { (getTime(time, it) > distance).toInt() }.toLong()
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 288L)
-//    check(part2(testInput) == 35)
+    check(part2(testInput) == 71503L)
 
     val input = readInput("Day06")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
