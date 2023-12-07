98613024bf0226d87e9a616c6c0d11f2cb485eed
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-07 21:36:56 +0100

    feat: day 7 part 1

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,18 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Long {
-        return 0
+    fun part1(input: List<String>): Int {
+        return input
+            .asSequence()
+            .map { it.split(" ") }
+            .map { (cards, bid) -> Hand(cards, bid.toInt()) }
+            .map { hand ->
+                hand to hand.cards
+                    .groupingBy { it }
+                    .eachCount()
+                    .values
+                    .sorted()
+                    .toHandType()
+            }.sortedWith(
+                compareByDescending<Pair<Hand, HandType?>> { (_, handType) -> handType }
+                    .thenBy { (hand, _) ->
+                        hand.cards
+                            .replace('T', 'V')
+                            .replace('J', 'W')
+                            .replace('Q', 'X')
+                            .replace('K', 'Y')
+                            .replace('A', 'Z')
+            }).withIndex().sumOf { (idx, pair) -> (idx+1) * pair.first.bid }
+
+
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Int {
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 0L)
-    check(part2(testInput) == 0L)
+    check(part1(testInput) == 6440)
+    check(part2(testInput) == 0)
 
     val input = readInput("Day07")
     part1(input).println()
     part2(input).println()
 }
+
+internal enum class HandType(val distribution: List<Int>) {
+    FiveOfAKind(listOf(5)),
+    FourOfAKind(listOf(1, 4)),
+    FullHouse(listOf(2, 3)),
+    ThreeOfAKind(listOf(1, 1, 3)),
+    TwoPair(listOf(1, 2, 2)),
+    OnePair(listOf(1, 1, 1, 2)),
+    HighCard(listOf(1, 1, 1, 1, 1));
+}
+
+internal fun List<Int>.toHandType() = HandType.entries.firstOrNull { this == it.distribution }
+
+internal data class Hand(val cards: String, val bid: Int)
