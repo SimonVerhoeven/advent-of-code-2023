f06044794c9a0d3d392e2ed5e19ecf23083e0577
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-07 16:46:18 +0100

    feat: day 5 part 2 - slow & brute

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,40 +1,62 @@
-fun main() {
+// Slow, brute method
 
-    fun createConversionMaps(input: List<String>): List<Map<LongRange, LongRange>> {
-        val conversionMaps = input
-                .drop(2)
-                .joinToString("\n")
-                .split("\n\n")
-                .map { segment ->
-                    segment.lines().drop(1).associate {
-                        it.split(" ").map { it.toLong() }.let { (dest, source, length) ->
-                            source..(source + length) to dest..(dest + length)
-                        }
+fun main() {
+    data class Mapping(val range: LongRange, val offset: Long)
+
+    fun createConversionMaps(input: List<String>): List<List<Mapping>> {
+        val iterator = input.drop(2).iterator()
+
+        return buildList {
+            while (iterator.hasNext()) {
+                check(iterator.next().endsWith("map:"))
+                var line = iterator.next()
+                val mappings = buildList {
+                    while (line.isNotBlank()) {
+                        val (destinationRangeStart, sourceRangeStart, rangeLength) = line.split(" ").filter { it.isNotBlank() }.map { it.toLong() }
+                        val offset = destinationRangeStart - sourceRangeStart
+                        add(Mapping(sourceRangeStart..<(sourceRangeStart + rangeLength), offset))
+                        line = (if (iterator.hasNext()) iterator.next() else "")
                     }
                 }
-        return conversionMaps
-    }
-
-    fun part1(input: List<String>): Long {
-        val seeds = input.first().substringAfter(" ").split(" ").map { it.toLong() }
-        val conversionMaps = createConversionMaps(input)
-
-        return seeds.minOf { seed ->
-            conversionMaps.fold(seed) { value, map ->
-                map.entries.firstOrNull { value in it.key }?.let { (source, dest) -> dest.first + (value - source.first) } ?: value
+                add(mappings)
             }
         }
     }
 
-    fun part2(input: List<String>): Long {
-       return 0;
+    fun part1(seedLine: String, conversionMaps: List<List<Mapping>>): Long {
+        return seedLine.substringAfter(" ").split(" ").map { it.toLong() }.minOf { seed ->
+            conversionMaps.fold(seed) { acc, mappings ->
+                val offset = mappings.find { acc in it.range }?.offset ?: 0
+                acc + offset
+            }
+        }
+    }
+
+    fun part2(seedLine: String, conversionMaps: List<List<Mapping>>): Long {
+        val seeds = seedLine.removePrefix("seeds: ").split(" ").filter { it.isNotBlank() }.map { it.toLong() }
+        return seeds
+            .asSequence()
+            .windowed(2, 2)
+            .flatMap { (start, length) -> start ..< (start + length) }
+            .minOf { seed ->
+                conversionMaps.fold(seed) { acc, mappings ->
+                    val offset = mappings.find { acc in it.range }?.offset ?: 0
+                    acc + offset
+                }
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 35L)
+    val testSeedLine = testInput.first()
+    val testConversionMappings = createConversionMaps(testInput)
+    check(part1(testSeedLine, testConversionMappings) == 35L)
+    check(part2(testSeedLine, testConversionMappings) == 46L)
 
     val input = readInput("Day05")
-    part1(input).println()
-    part2(input).println()
+    val seedLine = input.first()
+    val conversionMappings = createConversionMaps(input)
+
+    part1(seedLine, conversionMappings).println()
+    part2(seedLine, conversionMappings).println()
 }
