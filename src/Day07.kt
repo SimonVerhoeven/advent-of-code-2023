030751a074bba945af6b9d0945ffa61c30a280e7
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-08 17:54:43 +0100

    feat: day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -26,13 +26,43 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input
+            .asSequence()
+            .map { it.split(" ") }
+            .map { (cards, bid) -> Hand(cards, bid.toInt()) }
+            .map { hand ->
+                hand to hand.cards
+                    .groupingBy { it }
+                    .eachCount()
+                    .toList()
+                    .sortedByDescending { it.second }
+                    .toMap()
+                    .toMutableMap()
+            }.map numberCards@{ (hand, cardCount) ->
+                val first = cardCount.toList().firstOrNull { it.first != 'J' } ?: return@numberCards hand to HandType.FiveOfAKind
+                val joker = cardCount['J']
+                cardCount.replace(first.first, first.second + (joker ?: 0))
+                cardCount.remove('J')
+                val handType = cardCount.values.sorted().toHandType()
+                hand to handType
+            }.sortedWith(
+                compareByDescending<Pair<Hand, HandType?>> { (_, handType) -> handType }
+                    .thenBy { (hand, _) ->
+                        hand.cards
+                            .replace('A', 'Z')
+                            .replace('K', 'Y')
+                            .replace('Q', 'X')
+                            .replace('T', 'W')
+                            .replace('J', '1')
+                    }
+            ).withIndex()
+            .sumOf { (idx, pair) -> (idx+1) * pair.first.bid }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 6440)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
     part1(input).println()
