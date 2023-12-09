680d127eb1f61b8b75c892147e0d6ea46831dd23
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-12-09 11:06:06 +0100

    feat: day 8 part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,5 +1,24 @@
 fun main() {
+    fun createNetwork(input: List<String>) = input
+        .drop(2)
+        .map { Regex("[A-Z][A-Z][A-Z]").findAll(it).map { it.value } }
+        .associate { seq ->
+            val (a, b, c) = seq.toList()
+            a to (b to c)
+        }
+
     fun part1(input: List<String>): Int {
+        val instructions = input.first()
+        val network = createNetwork(input)
+
+        var current = "AAA"
+
+        0.rangeTo(Int.MAX_VALUE).forEach {
+            current = if ('L' == instructions[it % instructions.length]) network[current]!!.first else network[current]!!.second
+            if ("ZZZ" == current) {
+                return it + 1
+            }
+        }
         return 0
     }
 
@@ -9,7 +28,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 0)
+
+    check(part1(testInput) == 6)
     check(part2(testInput) == 0)
 
     val input = readInput("Day08")
