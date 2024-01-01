e3c003dc38ee306ee30a93366debe8af1a057297
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-01-01 11:17:42 +0100

    feat: expand 3d class

--- src/Point3D.kt
+++ src/Point3D.kt
@@ -1,2 +1,8 @@
 class Point3D(x: Int, y: Int, z: Int) {
+    companion object {
+        fun of(input: String): Point3D {
+            val (x, y, z) = input.split(',').map { it.trim().toInt() }
+            return Point3D(x, y, z)
+        }
+    }
 }
